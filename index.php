96667b2dd1f176c9788e80992846451cb82a88e3
Moshiur Rahman <unimrgm@gmail.com>
2016-11-08 21:59:10 +0600

    bootstrap template and jquery added to the repository

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Moshiur Rahman
+ * Date: 11/8/2016
+ * Time: 9:25 PM
+ */
+?>
+<html>
+<head>
+    <title>Moshiur Rahman</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+</head>
+<body>
+<div class="container">
+    
+</div>
+</body>
+</html>
